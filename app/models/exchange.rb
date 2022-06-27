ea9e633d7b47c94ad73abacaea17c9251068ccdf
jaime zuber <jaimezuber99@gmail.com>
2022-06-27 17:06:47 -0300

    mas validaciones

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,4 +1,5 @@
 class Exchange < ApplicationRecord
   belongs_to :user
   validates :read_key, :write_key, presence: true, length: { is: 64 }, uniqueness: true
+
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,4 +4,13 @@ class Subscription < ApplicationRecord
   has_many :positions
 
   # scope :trader_suscriptions, -> { where(current_user.id == self.subscriber_id) }
+
+  validate :amount_logic
+
+  private
+
+  def amount_logic
+    errors.add(:amount_per_trade, "can't be greate than your total amount") unless
+      amount_per_trade < max_amount
+  end
 end
