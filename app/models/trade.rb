92f29d9cb00a558bd8a370e9cc27d76e6d5230a8
jaime zuber <jaimezuber99@gmail.com>
2022-06-27 00:01:20 -0300

    validations of symbol and presence done

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,4 +1,15 @@
 class Trade < ApplicationRecord
   belongs_to :trader, class_name: "User", foreign_key: "trader_id"
   has_many :positions
+
+  before_validation :upcase
+  validates :symbol, inclusion: { in: ['BTC', 'ETH', 'BCH', 'ZEC', 'RCN', 'MTB18', 'LTC', 'RPC', 'UBI'],
+                                  message: `%{value} no es una criptomoneda listada` }
+  validates :symbol, :entry_price, :take_profit, :stop_loss, :side, presence: true
+
+  private
+
+  def upcase
+    symbol.upcase!
+  end
 end
