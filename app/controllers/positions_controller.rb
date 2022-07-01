042f90a886e61c25b2262a89ef000e691ea178d4
jaime zuber <jaimezuber99@gmail.com>
2022-06-30 21:50:01 -0300

    cambios con fer

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,8 +1,7 @@
 class PositionsController < ApplicationController
   def index
-    @positions = policy_scope(Position).select do |position|
-      position.subscription.subscriber == current_user
-    end
+    @positions = policy_scope(current_user.positions)
     @trades = current_user.trades
+    @recent_trades = current_user.trades.where.not(pnl: nil).order(created_at: :desc).limit(3).map(&:pnl)
   end
 end
