ac438350cee7c2d4c23a528794e9b3df3f2660b5
luca pagano <lpagano@udesa.edu.ar>
2022-07-04 17:48:32 -0300

    email fix

--- db/schema.rb
+++ db/schema.rb
@@ -46,11 +46,11 @@ ActiveRecord::Schema.define(version: 2022_06_29_001548) do
   create_table "bios", force: :cascade do |t|
     t.string "username"
     t.text "description"
-    t.float "rendimiento"
+    t.float "rendimiento", default: 0.0
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.float "volatilidad"
+    t.float "volatilidad", default: 0.0
     t.index ["user_id"], name: "index_bios_on_user_id"
   end
 
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2022_06_29_001548) do
     t.float "open_price"
     t.float "close_price"
     t.string "external_id"
+    t.float "pnl"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["subscription_id"], name: "index_positions_on_subscription_id"
@@ -79,6 +80,7 @@ ActiveRecord::Schema.define(version: 2022_06_29_001548) do
   create_table "subscriptions", force: :cascade do |t|
     t.integer "amount_per_trade"
     t.integer "max_amount"
+    t.float "rendimiento", default: 0.0
     t.bigint "trader_id", null: false
     t.bigint "subscriber_id", null: false
     t.datetime "created_at", precision: 6, null: false
