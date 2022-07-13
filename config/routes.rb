f0c50f2e5bc81a929cf51c73f5cb52502fd84cb8
Joel Jaldin <jsejaldin@gmail.com>
2022-07-12 22:53:04 -0300

    profile con funcionalidades

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   resources :positions, only: :index
   resources :trades, only: %i[new create]
   get 'profile', to: 'bios#profile', as: :profile
+  get 'subscribers', to: 'bios#subscribers', as: :subscribers
+  get 'subscriptors', to: 'bios#subscriptors', as: :subscriptors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
