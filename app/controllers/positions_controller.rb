85f6e5f7c8796ac00996a98333d7a02dc1a9f0d7
Jaimezuber <97305507+jaimezuber@users.noreply.github.com>
2022-07-14 21:35:32 -0300

    Update positions_controller.rb

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -4,11 +4,7 @@ class PositionsController < ApplicationController
     @trades = current_user.trades
     @recent_trades = current_user.trades.where.not(pnl: nil).order(created_at: :desc).limit(3).map(&:pnl)
 
-    require 'json'
-    require 'rest-client'
-    url = 'https://testnet.binancefuture.com/fapi/v1/premiumIndex'
-
-    response = RestClient.get url
+    response = RestClient.get 'https://testnet.binancefuture.com/fapi/v1/premiumIndex'
     @result = JSON.parse response.to_str
   end
 end
