9049b166d9a33ad5ec13aad5145dc6d8a8ff05d3
jaime zuber <jaimezuber99@gmail.com>
2022-07-19 20:17:28 -0300

    order redimiento

--- app/policies/bio_policy.rb
+++ app/policies/bio_policy.rb
@@ -2,7 +2,7 @@ class BioPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      scope.all
+      scope.all.order(rendimiento: :desc)
     end
   end
 
